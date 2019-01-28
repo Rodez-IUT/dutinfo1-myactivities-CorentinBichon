ebf8d167a71a0eb827a86956332c2bdc1f43a6f0
Franck Silvestre <franck.silvestre@gmail.com>
2019-01-28 14:41:34 +0100

    Fix 1.2 function to delete old activities OK

--- src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
+++ src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS integer AS $$
-  SELECT 1;
+CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS void AS $$
+  DELETE FROM activity WHERE modification_date < old_date;
 $$ LANGUAGE SQL;
